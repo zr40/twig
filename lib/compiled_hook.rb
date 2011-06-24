4ee95ebf4ec56b5362ff26c9c408742c970b237c
Matthijs van der Vleuten <zr40.nl@gmail.com>
2011-06-24 23:35:29 +0200

    Print notice and exit if Rubinius supports CodeLoader.compiled_hook, so that this can be removed once that happens.

--- lib/compiled_hook.rb
+++ lib/compiled_hook.rb
@@ -3,20 +3,27 @@
 # Rubinius doesn't currently have a hook for that.
 #
 # Remove this code when the hook is added.
-module Rubinius
-  class CodeLoader
-    @compiled_hook = Rubinius::Hook.new
 
-    class << self
-      attr_reader :compiled_hook
-    end
+if Rubinius::CodeLoader.respond_to? :compiled_hook
+  puts 'Twig development notice: Rubinius appears to support CodeLoader.compiled_hook.'
+  puts 'compiled_hook.rb can now be removed from Twig.'
+  exit 1
+else
+  module Rubinius
+    class CodeLoader
+      @compiled_hook = Rubinius::Hook.new
 
-    alias load_file_without_hook load_file
+      class << self
+        attr_reader :compiled_hook
+      end
 
-    def load_file
-      result = load_file_without_hook
-      CodeLoader.compiled_hook.trigger! result
-      result
+      alias load_file_without_hook load_file
+
+      def load_file
+        result = load_file_without_hook
+        CodeLoader.compiled_hook.trigger! result
+        result
+      end
     end
   end
 end
